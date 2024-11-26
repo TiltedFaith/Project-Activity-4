e5c7461fb897a2e7c85b8b5112d84d86e706e5b1
Ronald Barnachea <ronaldmichael.barnachea.cics@ust.edu.ph>
2024-11-26 21:22:25 +0800

    15

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,12 @@
 // eslint.config.js
-import eslintRecommended from 'eslint/conf/eslint-recommended.js';
-
 export default [
-  eslintRecommended,  // Use the ESLint recommended config
-  {
-    files: ['**/*.js'],
-    rules: {
-      'no-console': 'warn',  // Custom rules
-      'no-unused-vars': 'warn',
+    {
+      files: ['**/*.js'],
+      extends: ['eslint:recommended'],  // Use ESLint's built-in recommended rules
+      rules: {
+        'no-console': 'warn',  // Custom rules
+        'no-unused-vars': 'warn',
+      },
     },
-  },
-];
+  ];
+  
