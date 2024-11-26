dd0182d583a023cdaf97ffb476951aa98101aca2
Ronald Barnachea <ronaldmichael.barnachea.cics@ust.edu.ph>
2024-11-26 21:13:02 +0800

    Test 9

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,13 @@
-module.exports = {
-    extends: 'eslint:recommended', // Or your preferred ESLint config
+// eslint.config.js
+import eslintRecommended from 'eslint/conf/eslint-recommended.js';
+
+export default [
+  eslintRecommended,  // Use the ESLint recommended config
+  {
+    files: ['**/*.js'],
     rules: {
       'no-console': 'warn',
-      'no-unused-vars': 'warn'
-    }
-  };
-  
+      'no-unused-vars': 'warn',
+    },
+  },
+];
