5697de0833708af0ec580a98bd427d47dea4a377
Ronald Barnachea <ronaldmichael.barnachea.cics@ust.edu.ph>
2024-11-26 21:24:25 +0800

    17

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,15 @@
 // eslint.config.js
 import { ESLint } from 'eslint';
 
-const eslint = new ESLint();
-
 export default [
   {
     files: ['**/*.js'],
-    // Directly include the recommended config objects here
-    parserOptions: {
-      ecmaVersion: 'latest', // Use the latest ECMAScript version
-      sourceType: 'module',  // Ensure support for ES modules
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest', // Use the latest ECMAScript version
+        sourceType: 'module',  // Ensure support for ES modules
+      },
     },
-    plugins: ['eslint-plugin'],
     rules: {
       'no-console': 'warn', // Custom rules
       'no-unused-vars': 'warn',
